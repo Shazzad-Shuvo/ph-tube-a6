a1b61e01c9bd2ebd355e8124405e02fb70dddee9
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2023-08-31 22:51:34 +0600

    loaded video categories on button tab

--- js/phero-tube.js
+++ js/phero-tube.js
@@ -1,1 +1,25 @@
-console.log('phero')
+const handleCategory = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await res.json();
+    const categories = data.data;
+    // console.log(categories);
+    showCategory(categories);
+}
+
+const showCategory = (categories) =>{
+    const categoryTabContainer = document.getElementById('tab-container');
+
+    categories.forEach(category =>{
+        const categoryDiv = document.createElement('div');
+        categoryDiv.innerHTML = `
+        <button class="px-5 py-3 rounded-lg font-medium bg-slate-200 hover:bg-red-500 text-gray-800 hover:text-white ">${category?.category}</button>
+        `;
+
+        categoryTabContainer.appendChild(categoryDiv);
+    })
+}
+
+
+
+
+handleCategory();
